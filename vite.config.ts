6bddb84df1e4aef672a6fea51a4ed401fd069bc5
Emma-Hyejin <cjd191058@naver.com>
2023-11-17 17:24:15 +0900

    Chore: 프리티어 설정 + vite.config.ts 설정 + 공용 색상 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sass from "sass";
+import path from "path";
 
 // https://vitejs.dev/config/
+//[sass in react,vite]-https://harbiola.hashnode.dev/setting-up-vite-with-react-sass-and-typescript
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
+	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				implementation: sass,
+				additionalData: `@import "@/variable.scss";\n`
+			}
+		}
+	}
 });
